522d22803c6c849e79451d876d2475e4203cdb16
Zairxon <157012207+Zairxon@users.noreply.github.com>
2025-06-22 23:56:21 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from telegram.ext import Application, CommandHandler, CallbackQueryHandler, Mess
 
 # Конфигурация бота
 TOKEN = os.getenv('BOT_TOKEN')
-ADMIN_ID = int(os.getenv('ADMIN_ID', '0'))
+ADMIN_ID = 295608267
 DB_FILE = 'licenses.db'
 
 # Цена полной лицензии в долларах
